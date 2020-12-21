65e638067e5722bc6bb6aaaaffb64d22972b5f2f
Brando D <brando2147@gmail.com>
2020-12-21 15:24:04 -0700

    updated click event, updating API logic

--- script.js
+++ script.js
@@ -1,55 +1,68 @@
 $(document).ready(function () {
 
+    let currentDay = moment().format('L');
     var apiKey = "appid=797daeffdb6701215dfbc71cc3c18753";
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=tempe&appid=797daeffdb6701215dfbc71cc3c18753"
-    var userInput = "tempe";
+    // var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userCity + "&appid=797daeffdb6701215dfbc71cc3c18753"
+    // var userInput = "tempe";
     let days = ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5"];
 
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-
-        let temperature = response.main.temp
-        let humidity = response.main.humidity
-        let windSpeed = response.wind.speed
-        let icon = response.weather[0].icon
 
 
-        console.log(response)
+    // Retrieve weather data from API when City Name is submitted 
+    function getWeatherData(cityName) {
 
-        $('h3.city').text()
-        $('p.temp').text(temperature)
-        $('p.humidity').text(humidity)
-        $('p.windspeed').text(windSpeed)
+        console.log("here")
 
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
 
-        // Generate 5 day forecase 
-        $('#WeekForecast').text("5 Day Forecast");
-
-        for (let i = 0; i < days.length; i++) {
-
-            let card = $('<div class="card">');
-            let dayDate = $('<p ="card-title">')
-            let dayIcon = $('<img>')
-            let dayTemp = $('<p = "card-text">')
-            let dayHumidity = $('<p = "card-text">')
-
-            dayDate.text('date')
-            dayIcon.text('icon')
-            dayTemp.text(temperature)
-            dayHumidity.text(humidity)
-
-            $('#WeekForecast').append(card);
-            card.append(dayDate, dayIcon, dayTemp, dayHumidity);
-
-        }
+            let temperature = response.main.temp
+            let humidity = response.main.humidity
+            let windSpeed = response.wind.speed
+            let icon = response.weather[0].icon
 
 
+            $("#cityTimeDisplay").text(cityName + " " + currentDay)
+            $("#tempDisplay").text("Temperature: " + temperature)
+            $("#humidityDisplay").text("Humidity: " + humidity)
+            $("#windspeedDisplay").text("Wind Speed: " + windSpeed)
 
-    });
+
+            console.log(response)
 
 
+            // Generate 5 day forecast 
+            $('#WeekForecast').text("5 Day Forecast");
 
+            for (let i = 0; i < days.length; i++) {
+
+                let card = $('<div class="card">');
+                let dayDate = $('<p ="card-title">')
+                let dayIcon = $('<img>')
+                let dayTemp = $('<p = "card-text">')
+                let dayHumidity = $('<p = "card-text">')
+
+                dayDate.text(currentDay)
+                dayIcon.text('icon')
+                dayTemp.text(temperature)
+                dayHumidity.text(humidity)
+
+                $('#WeekForecast').append(card);
+                card.append(dayDate, dayIcon, dayTemp, dayHumidity);
+            }
+
+
+        });
+
+        // Retrieves data after clicking search button 
+        $(".btn").on('click', function () {
+            console.log("here2")
+            cityName = $(this).siblings("#inputValue").val();
+            getWeatherData(cityName);
+        })
+
+    }
 });
